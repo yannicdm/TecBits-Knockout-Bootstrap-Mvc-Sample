721c0b88863faccb0865a8d31a46dbce9ced4fcf
Yannic De Meyer <ydemeyer@gmail.com>
2014-12-02 14:10:44 +0100

    Tryout > load via requirejs

--- TecBits-Knockout-Bootstrap-Mvc-Sample/Scripts/init.js
+++ TecBits-Knockout-Bootstrap-Mvc-Sample/Scripts/init.js
@@ -1,8 +1,13 @@
 ﻿/// init.js
 /// <reference path="require.js" />
 /// <reference path="knockout-3.2.0.js" />
+/// <reference path="ViewModel/MyMath.js" />
 
 
-require(['knockout', 'ViewModel/ArticlesVM'], function (ko, ArticlesVM) {
-    ko.applyBindings(new ArticlesVM());
-});
+//require(['knockout', 'ViewModel/ArticlesVM'], function (ko, ArticlesVM) {
+//    ko.applyBindings(new ArticlesVM());
+//});
+
+//require(['MyMath'], function (MyMath) {
+//    console.log(MyMath.add(5,5));
+//});

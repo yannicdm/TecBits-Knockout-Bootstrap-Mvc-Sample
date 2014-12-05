5e6b2469af0f8a13903ee67549a435192dfcc266
Yannic De Meyer <ydemeyer@gmail.com>
2014-12-05 19:35:02 +0100

    Rename ArticleVm

--- TecBits-Knockout-Bootstrap-Mvc-Sample/Scripts/ViewModel/ArticlesVM.js
+++ TecBits-Knockout-Bootstrap-Mvc-Sample/Scripts/ViewModel/ArticlesVM.js
@@ -1,6 +1,8 @@
 ﻿/// ArticlesVM.js
 /// <reference path="../knockout-3.2.0.js" />
+/// <reference path="../require.js" />
 
+<<<<<<< HEAD
 
 $(document).ready(function () {
     ko.applyBindings(IndexViewModel);
@@ -13,13 +15,23 @@ function IndexViewModel() {
     self.article = ko.observable();
     self.choosenArticle = ko.observable();
     self.apiUrl = 'http://localhost:50447/api/v1/articles/';
+=======
+    function ArticlesViewModel() {
+        // data
+        var self = this;
+        self.articles = ko.observable();
+        self.article = ko.observable();
+        self.choosenArticle = ko.observable();
+        self.apiUrl = 'http://localhost:50447/api/v1/articles/';
+>>>>>>> origin/master
 
-    self.getArticles = function () {
-        $.get(self.apiUrl).done(function (data) {
-            self.articles(data);
-        });
-    };
+        self.getArticles = function () {
+            $.get(self.apiUrl).done(function (data) {
+                self.articles(data);
+            });
+        };
 
+<<<<<<< HEAD
     self.getArticle = function (article) {
         $.get(self.apiUrl + article.Id).done(function (data) {
             self.article(data);
@@ -31,3 +43,15 @@ function IndexViewModel() {
 };
 
 
+=======
+        self.getArticle = function (article) {
+            $.get(self.apiUrl + article.Id).done(function (data) {
+                self.article(data);
+            });
+        };
+
+        self.getArticles();
+};
+
+ko.applyBindings(ArticlesViewModel);
+>>>>>>> origin/master
